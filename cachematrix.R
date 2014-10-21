1df15c6c3a790a74e6b271d708e5ce5af9999d85
ChauNamKyNguyen <kychau.info@gmail.com>
2014-10-21 10:05:36 +0700

    First create

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author : Chau Nguyen
+## Created Date : 21st Oct, 2014
+## Description : 
+##      Ver 1.0 : New create
+##        + makeCacheMatrix : will create a special matrix to store the matrix's invert result in cache
+##        + cacheSolve : will compute the invert of a matrix if this invert doesn't exist. Else, the invert result will be
+##                       restore from a cache.
 
-## Write a short comment describing this function
+## Create four functions of the matrix x.
+##     + set : to set value for the matrix in x and reset the invert value of the matrix
+##     + get : get the invert value of the matrix
+##     + setinvert : set the invert value of a matrix in a cache
+##     + getinvert : get the invert value of a matrix in a cache
+## Return: return a list of four created functions  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  
+  set <- function (y) {
+    x <<- y
+    i <<- NULL
+  }
+  
+  get <- function () x
+  
+  setinvert <- function (invert) i <<- invert
+  getinvert <- function () i
+  
+  list (set = set, get = get, setinvert = setinvert, getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Compute the invert of a matrix if this value doesn't exist. Else, restore the invert value from the matrix's cache
+## Return : Return the invest value of a "special" matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  i <- x$getinvert ()
+  
+  if (!is.null(i)) {
+    message ("getting cached data")
+    return (i)
+  }
+  
+  data <- x$get()
+  message ("calculate the data from begining")
+  i <- solve (data, ...)
+  
+  x$setinvert (i)
+  
+  ## Return a matrix that is the inverse of 'x'
+  i
 }
